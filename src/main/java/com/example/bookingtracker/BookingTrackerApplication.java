765a47a71d5caa3b550b585879f9c64f48bd55a5
Strahil Peykov <strahil.peykov@gmail.com>
2025-03-25 08:18:01 +0100

    Initial project setup with React frontend

---
+++ src/main/java/com/example/bookingtracker/BookingTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookingtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookingtracker/BookingTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookingtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
